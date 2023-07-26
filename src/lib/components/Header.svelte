9d4dfa2d37f0c8799e40f49961370ece3ea8c4f6
Minh Truong <minhtruong@precocityllc.com>
2023-07-26 15:12:42 -0500

    Responsive header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <div class="flex items-center bg-teal-200 py-4">
-	<Container class="flex flex-row items-center gap-10 text-blue-950">
-		<div class="text-3xl font-bold mr-5">JTC Demo</div>
+	<Container class="flex flex-row items-center gap-4 text-blue-950 sm:gap-10">
+		<div class="text-2xl font-bold">JTC Demo</div>
 
 		<a href="/companies" class="text-lg hover:underline">Companies</a>
 		<a href="/books" class="text-lg hover:underline">Books</a>
