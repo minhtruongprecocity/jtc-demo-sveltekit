3f9cc5943820917bcc42170433e87a53623a7a29
Minh Truong <minhtruong@precocityllc.com>
2023-07-27 21:45:23 -0500

    Custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+	import Container from '$lib/components/Container.svelte';
+</script>
+
+<Container>
+	<h1 class="text-xl">{$page.error?.message}</h1>
+</Container>
